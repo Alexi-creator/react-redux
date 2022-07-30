4a8397a2ea42ea0e1812d996e29dc62d9b9a2b45
Ilya <ilya.p@finanso.com>
2022-07-30 14:03:52 +0300

    add debounce for search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,38 @@
 import React from 'react'
 import styles from './Search.module.scss'
 import { SearchInputProps } from './Search.props'
+import debounce from 'lodash.debounce'
 
 import SearchImg from '../../assets/images/SearchImg'
 import Close from '../../assets/images/Close'
 
 export const Search: React.FC<SearchInputProps> = ({
-  searchValue,
   setSearchValue,
   placeholder,
 }) => {
+  const [value, setValue] = React.useState('')
+
   const searchRef = React.useRef<HTMLInputElement>(null)
+
+  // при перерендере компонента useCallback сохраняет ссылку на функцию
+  const updateSearchValue = React.useCallback(
+    debounce((str: string) => {
+      setSearchValue(str)
+    }, 500),
+    []
+  )
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+    updateSearchValue(e.target.value)
+  }
+
   const clearInput = () => {
+    setValue('')
     setSearchValue('')
     searchRef.current?.focus()
   }
+
   return (
     <div className={styles.root}>
       <SearchImg width={20} className={styles.iconSearch} />
@@ -22,12 +40,10 @@ export const Search: React.FC<SearchInputProps> = ({
         ref={searchRef}
         className={styles.input}
         placeholder={placeholder}
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={value}
+        onChange={onChangeInput}
       />
-      {searchValue && (
-        <Close onClick={clearInput} className={styles.closeIcon} />
-      )}
+      {value && <Close onClick={clearInput} className={styles.closeIcon} />}
     </div>
   )
 }
